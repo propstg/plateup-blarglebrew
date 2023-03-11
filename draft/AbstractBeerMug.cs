71f1ef3f14977d7166f58feb2dd858f64ba00c2c
propstg <g@prop.st>
2023-03-11 16:25:59 -0500

    add a cold effect to beer mugs

--- draft/AbstractBeerMug.cs
+++ draft/AbstractBeerMug.cs
@@ -2,7 +2,9 @@
 using KitchenData;
 using KitchenLib.Customs;
 using KitchenLib.Utils;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.VFX;
 
 namespace BlargleBrew.draft {
 
@@ -18,10 +20,26 @@ namespace BlargleBrew.draft {
         public override GameObject Prefab => BlargleBrewMod.bundle.LoadAsset<GameObject>(prefabName);
         public override string UniqueNameID => $"BlargleBrew - Mug {name}";
 
+        public const string VFX_NAME = "Freezer Vapour";
+
         public override void OnRegister(GameDataObject gdo) {
             MaterialUtils.ApplyMaterial(Prefab, "glass", CommonMaterials.Mug.glass);
             MaterialUtils.ApplyMaterial(Prefab, "beer", beerMaterial);
             MaterialUtils.ApplyMaterial(Prefab, "foam", foamMaterial);
+            GameObject vfxGameObject = new GameObject($"Beer VFX - {VFX_NAME}");
+            VisualEffectAsset asset = Resources.FindObjectsOfTypeAll<VisualEffectAsset>().Where(vfx => vfx.name == VFX_NAME).FirstOrDefault();
+            if (asset != default) {
+                VisualEffect vfx = vfxGameObject.GetComponent<VisualEffect>();
+                if (vfx == null) {
+                    vfx = vfxGameObject.AddComponent<VisualEffect>();
+                }
+                vfx.visualEffectAsset = asset;
+
+                vfxGameObject.transform.parent = Prefab.transform;
+                vfxGameObject.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                vfxGameObject.transform.localPosition = Vector3.zero;
+                vfxGameObject.transform.rotation = Quaternion.identity;
+            }
         }
     }
 }
